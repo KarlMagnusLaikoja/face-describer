36eddbf47cc17225532e969dfd0481b3eb14f3b3
KarlMagnusLaikoja <karl.magnus.laikoja@gmail.com>
2023-10-25 13:51:19 +0300

    move api call to description component

--- src/frontend/src/components/ImagePicker.vue
+++ src/frontend/src/components/ImagePicker.vue
@@ -17,47 +17,7 @@
 </template>
 
 <script>
-const toBase64 = file => new Promise((resolve, reject) => {
-    const reader = new FileReader();
-    reader.readAsDataURL(file);
-    reader.onload = () => resolve(reader.result);
-    reader.onerror = reject;
-});
-
-async function describe(event){
-    //File to base64
-    var image = await toBase64(event.files[0]);
-    console.log(image);
-
-    //Get language from store
-    const language = this.$store.state.languageCode;
-
-    //Send description request to backend
-    var response = await fetch("/describe", {
-      method: "POST",
-      body: JSON.stringify({
-        "language": language,
-        "image": image
-      }),
-      headers: {
-        "Content-type": "application/json;charset=UTF-8"
-      }
-    });
-    response = await response.json();
-
-    //Propagate any errors
-    if (response.errorCode != 0){
-        document.getElementById('errorMessageContainer').style.display='';
-        document.getElementById('errorMessage').innerHTML = response.errorMessage;
-        return;
-    }
-
-
-    //Propagate result to description field
-    //TODO
-    console.log(response);
-}
-
+import {describe} from './Description.vue';
 
 
 export default {
